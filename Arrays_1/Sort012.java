33d9148539c2df8a47664961b94f8c9046cbf354
Nizamuddin Mondal <nizam420nizam@gmail.com>
2022-06-05 00:57:41 +0530

    adding sort012

---
+++ Arrays_1/Sort012.java
@@ -0,0 +1,34 @@
+class Solution {
+    public void sortColors(int[] nums) {
+        int lo = 0;
+        int hi = nums.length - 1;
+        int mid = 0;
+        int temp;
+        while (mid <= hi){
+            switch(nums[mid]){
+                case 0:{
+                    temp = nums[lo];
+                    nums[lo] = nums[mid];
+                    nums[mid] = temp;
+                    lo++;
+                    mid++;
+                    break;
+                }
+                case 1:{
+                    mid++;
+                    break;
+                }
+                case 2:{
+                    temp = nums[mid];
+                    nums[mid] = nums[hi];
+                    nums[hi] = temp;
+                    hi--;
+                    break;
+                }
+            }
+        }
+    }
+}
+
+// Time Complexity: O(N)
+// Space Complexity: O(1)

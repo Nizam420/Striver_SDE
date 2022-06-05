4251ea63c670225b576cd878891da64608317417
Nizamuddin Mondal <nizam420nizam@gmail.com>
2022-06-05 13:08:12 +0530

    adding rotate matrix

---
+++ Arrays_2/RotateMat.java
@@ -0,0 +1,23 @@
+class Solution {
+    public void rotate(int[][] matrix) {
+        for (int i = 0; i < matrix.length; i++){
+            for (int j = i; j < matrix[0].length; j++){
+                int temp = 0;
+                temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+        for (int i = 0; i < matrix.length; i++){
+            for (int j = 0; j < matrix.length / 2; j++){
+                int temp = 0;
+                temp = matrix[i][j];
+                matrix[i][j] = matrix[i][matrix.length - 1 - j];
+                matrix[i][matrix.length - 1 - j] = temp;
+            }
+        }
+    }
+}
+
+// Time Complexity: O(N*N) + O(N*N).One O(N*N) for transposing the matrix and the other for reversing the matrix.
+// Space Complexity: O(1).

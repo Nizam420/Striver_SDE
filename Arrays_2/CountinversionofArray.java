0c7c1cca2b9f28e8bbe150e8f98f2288be3b5713
Nizamuddin Mondal <nizam420nizam@gmail.com>
2022-06-07 00:42:58 +0530

    fadding count inversion of array

---
+++ Arrays_2/CountinversionofArray.java
@@ -0,0 +1,51 @@
+
+import java.util.Arrays;
+
+public class CountinversionofArray {
+    private static int mergeAndCount(int[] arr, int l, int m, int r) {
+        int[] left = Arrays.copyOfRange(arr, l, m + 1);
+        int[] right = Arrays.copyOfRange(arr, m + 1,r + 1);
+
+        int i = 0, j = 0, k = l, swaps = 0;
+
+        while (i < left.length && j < right.length) {
+            if (left[i] <= right[j])
+                arr[k++] = left[i++];
+            else {
+                arr[k++] = right[j++];
+                swaps += (m + 1) - (l + i);
+            }
+        }
+        while (i < left.length)
+            arr[k++] = left[i++];
+        while (j < right.length)
+            arr[k++] = right[j++];
+        return swaps;
+    }
+
+    private static int mergeSortAndCount(int[] arr,int l, int r) {
+        int count = 0;
+
+        if (l < r) {
+            int m = (l + r) / 2;
+            count += mergeSortAndCount(arr, l, m);
+
+            
+            count += mergeSortAndCount(arr, m + 1, r);
+
+            
+            count += mergeAndCount(arr, l, m, r);
+        }
+
+        return count;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 1, 20, 6, 4, 5 };
+
+        System.out.println(mergeSortAndCount(arr, 0,arr.length - 1));
+    }
+}
+
+// Time Complexity: O(n log n), The algorithm used is divide and conquer, So in each level, one full array traversal is needed, and there are log n levels, so the time complexity is O(n log n).
+// Space Complexity: O(n), Temporary array.

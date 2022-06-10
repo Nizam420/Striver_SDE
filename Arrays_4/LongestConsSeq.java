8228c77dbb88f062d211bc1eb02f125c0798d572
Nizamuddin Mondal <nizam420nizam@gmail.com>
2022-06-10 21:43:17 +0530

    adding longest consecutive sequence

---
+++ Arrays_4/LongestConsSeq.java
@@ -0,0 +1,26 @@
+import java.util.*;
+class Solution {
+    public int longestConsecutive(int[] nums) {
+        Set<Integer> hs = new HashSet<>();
+        for (int num : nums){
+            hs.add(num);
+        }
+        int ans = 0;
+        for (int num : nums){
+            if (!hs.contains(num-1)){
+                int curNum = num;
+                int curSt = 1;
+                while (hs.contains(curNum + 1)){
+                    curNum +=1;
+                    curSt += 1;
+                }
+                ans = Math.max(ans, curSt);
+            }
+        }
+        return ans;
+    }
+}
+
+// Time Complexity: O(3N)
+
+// Space Complexity: O(1)

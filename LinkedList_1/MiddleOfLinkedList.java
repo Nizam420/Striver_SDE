35301e29e4362b369768b59f1ca20903433ba5c7
Nizamuddin Mondal <nizam420nizam@gmail.com>
2022-06-12 16:23:40 +0530

    adding middle of the linked list and sc and tc

--- LinkedList_1/MiddleOfLinkedList.java
+++ LinkedList_1/MiddleOfLinkedList.java
@@ -1,12 +1,12 @@
 
  
-  public class ListNode {
-      int val;
-      ListNode next;
-      ListNode() {}
-      ListNode(int val) { this.val = val; }
-      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
-  }
+//   public class ListNode {
+//       int val;
+//       ListNode next;
+//       ListNode() {}
+//       ListNode(int val) { this.val = val; }
+//       ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+//   }
  
 class Solution {
     public ListNode middleNode(ListNode head) {
@@ -20,3 +20,7 @@ class Solution {
         return slow;
     }
 }
+
+// Time Complexity: O(N/2)
+
+// Space Complexity: O(1)

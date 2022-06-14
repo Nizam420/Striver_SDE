53040ca723260074c1a11e14e387ef0dcb00520f
Nizamuddin Mondal <nizam420nizam@gmail.com>
2022-06-14 23:23:33 +0530

    adding find starting point of loop

---
+++ LinkedList_2/FindStartingPointofLoop.java
@@ -0,0 +1,28 @@
+class Solution {
+    public ListNode detectCycle(ListNode head) {
+        if (head == null || head.next == null){
+            return null;
+        }
+        ListNode slow = head;
+        ListNode fast = head;
+        ListNode entry = head;
+        
+        while (fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+            
+            if (slow == fast){
+                while (slow != entry){
+                    slow = slow.next;
+                    entry = entry.next;
+                }
+                return entry;
+            }
+        }
+        return null;
+    }
+}
+
+// Time Complexity: O(N)
+
+// Space Complexity: O(1)

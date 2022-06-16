f78e56b165bd5e53014bc1b577971bbe7c49f6d3
Nizamuddin Mondal <nizam420nizam@gmail.com>
2022-06-16 16:30:45 +0530

    adding N meeting in one room again

--- Greedy_Algo/Nmeeting.java
+++ Greedy_Algo/Nmeeting.java
@@ -29,8 +29,7 @@ class meetingC implements Comparator<meeting>{
 
 class Solution 
 {
-    //Function to find the maximum number of meetings that can
-    //be performed in a meeting room.
+    
     public static int maxMeetings(int start[], int end[], int n)
     {
         // add your code here

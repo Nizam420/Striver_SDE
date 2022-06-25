4452c88fc2ab6b1c1ec6f8a5a6b08139bfbcafe3
Nizamuddin Mondal <nizam420nizam@gmail.com>
2022-06-25 16:20:23 +0530

    Adding Nth root of M

---
+++ Binary_Search/NthRootOfm.java
@@ -0,0 +1,33 @@
+
+public class NthRootOfm {
+    private static double multiply(double number, int n) {
+        double ans = 1.0;
+        for (int i = 1; i <= n; i++) {
+            ans = ans * number;
+        }
+        return ans;
+    }
+
+    private static void getNthRoot(int n, int m) {
+        double low = 1;
+        double high = m;
+        double eps = 1e-6;
+
+        while ((high - low) > eps) {
+            double mid = (low + high) / 2.0;
+            if (multiply(mid, n) < m) {
+                low = mid;
+            } else {
+                high = mid;
+            }
+        }
+
+        System.out.println(n + "th root of " + m + " is " + low);
+
+    }
+
+    public static void main(String[] args) {
+        int n = 3, m = 27;
+        getNthRoot(n, m);
+    }
+}

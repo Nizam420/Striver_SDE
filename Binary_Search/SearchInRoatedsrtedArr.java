52758cf1558d043a88feca8660f0c29d6f817ce8
Nizamuddin Mondal <nizam420nizam@gmail.com>
2022-06-26 10:26:27 +0530

    Adding Search in roated sorted array

---
+++ Binary_Search/SearchInRoatedsrtedArr.java
@@ -0,0 +1,37 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int low = 0;
+        int high = nums.length - 1;
+        while (low <= high) {
+            int mid = (low + high) >> 1;
+            if (nums[mid] == target) {
+                return mid;
+            }
+
+            if (nums[low] <= nums[mid]) {
+                if (nums[low] <= target && target <= nums[mid]) {
+                    high = mid - 1;
+                } else {
+                    low = mid + 1;
+                }
+            } else {
+                if (nums[mid] <= target && target <= nums[high]) {
+                    low = mid + 1;
+                } else {
+                    high = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
+}
+
+// Time Complexity: O(log(N))
+
+// Reason: We are performing a binary search, this turns time complexity to
+// O(log(N)) where N is the size of the array.
+
+// Space Complexity: O(1)
+
+// Reason: We do not use any extra data structure, this turns space complexity
+// to O(1).

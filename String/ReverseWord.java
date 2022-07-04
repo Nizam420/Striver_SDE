8d69904a13547f29de9c435ef2689a062541a990
Nizamuddin Mondal <nizam420nizam@gmail.com>
2022-07-04 13:08:46 +0530

    Adding Reverse a word in string

---
+++ String/ReverseWord.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+public class ReverseWord {
+    public String reverseWords(String s) {
+        int left = 0;
+        int right = s.length() - 1;
+
+        String temp = "";
+        String ans = "";
+
+        while (left <= right) {
+            char ch = s.charAt(left);
+            if (ch != ' ') {
+                temp += ch;
+            } else if (ch == ' ' && !temp.equals("")) {
+                if (!ans.equals("")) {
+                    ans = temp + " " + ans;
+                } else {
+                    ans = temp;
+                }
+                temp = "";
+            }
+            left++;
+        }
+        if (!temp.equals("")) {
+            if (!ans.equals("")) {
+                ans = temp + " " + ans;
+            } else {
+                ans = temp;
+            }
+        }
+        return ans;
+    }
+}
+
+// Time Complexity: O(N), N~length of string
+
+// Space Complexity: O(1), Constant Space

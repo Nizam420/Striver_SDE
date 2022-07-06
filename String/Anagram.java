6b56e0bdeff6de5716ab9fba349a47ac5c071d7f
Nizamuddin Mondal <nizam420nizam@gmail.com>
2022-07-06 12:58:19 +0530

    Adding Valid Anagram

---
+++ String/Anagram.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()){
+            return false;
+        }
+        char[] sArr = s.toCharArray();
+        char[] tArr = t.toCharArray();
+        
+        Arrays.sort(sArr);
+        Arrays.sort(tArr);
+        
+        for (int i = 0; i < s.length(); i++){
+            if (sArr[i] != tArr[i]){
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
+//Time complexity: O(NlogN)
+//Space complexity: O(N)

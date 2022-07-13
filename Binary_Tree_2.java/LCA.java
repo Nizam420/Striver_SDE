7b3d1b2a25c913a0957fe477573b6482ec91eefa
Nizamuddin Mondal <nizam420nizam@gmail.com>
2022-07-13 17:40:36 +0530

    Adding LCA of a tree again

--- Binary_Tree_2.java/LCA.java
+++ Binary_Tree_2.java/LCA.java
@@ -1,23 +1,20 @@
 class Solution {
     public Node lowestCommonAncestor(Node root, Node p, Node q) {
-        if (root == null || root == p || root == q){
+        if (root == null || root == p || root == q) {
             return root;
         }
         Node left = lowestCommonAncestor(root.left, p, q);
         Node right = lowestCommonAncestor(root.right, p, q);
-        
-        if (left == null){
+
+        if (left == null) {
             return right;
-        }
-        else if (right == null){
+        } else if (right == null) {
             return left;
-        }
-        else{
+        } else {
             return root;
         }
     }
 }
-
 
 // Time complexity: O(N)
 // Space complexity: O(N)

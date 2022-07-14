ba529b4978017a0904c6729c4b77d6f624c2ef7f
Nizamuddin Mondal <nizam420nizam@gmail.com>
2022-07-14 21:44:49 +0530

    Adding max path sum

---
+++ Binary_Tree_3/MaxPathsum.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int maxPathSum(Node root) {
+        int[] maxVal = new int[1];
+        maxVal[0] = Integer.MIN_VALUE;
+        pathSum(root, maxVal);
+        return maxVal[0];
+    }
+    public int pathSum(Node root, int[] maxVal){
+        if (root == null){
+            return 0;
+        }
+        int left = Math.max(0, pathSum(root.left, maxVal));
+        int right = Math.max(0, pathSum(root.right, maxVal));
+        
+        maxVal[0] = Math.max(maxVal[0], left + right + root.val);
+        
+        return Math.max(left, right) + root.val;
+    }
+}
+
+// Time complexity: O(N)
+// Space complexity: O(N)

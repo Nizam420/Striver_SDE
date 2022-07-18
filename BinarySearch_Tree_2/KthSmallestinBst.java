d51e4c2c4306c1e8de00faae865d5870e634af18
Nizamuddin Mondal <nizam420nizam@gmail.com>
2022-07-18 21:41:41 +0530

    Adding kth smallest element in bst

---
+++ BinarySearch_Tree_2/KthSmallestinBst.java
@@ -0,0 +1,25 @@
+public class KthSmallestinBst {
+    static int ans = 0;
+    static int cnt = 0;
+
+    public static int kthSmallest(Node root, int k) {
+        inOrder(root, k);
+        return ans;
+    }
+
+    public static void inOrder(Node root, int k) {
+        if (root == null) {
+            return;
+        }
+        inOrder(root.left, k);
+        cnt++;
+        if (cnt == k) {
+            ans = root.data;
+            return;
+        }
+        inOrder(root.right, k);
+    }
+}
+
+// Time complexity: O(N)
+// Space complexity: O(N)

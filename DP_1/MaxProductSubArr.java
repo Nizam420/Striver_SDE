cef78ae234d16a331ddb7ed38892c95d142c6bef
Nizamuddin Mondal <nizam420nizam@gmail.com>
2022-07-24 17:19:10 +0530

    Adding maximum product subarray

---
+++ DP_1/MaxProductSubArr.java
@@ -0,0 +1,30 @@
+import java.util.*;
+public class Main
+{
+	static int maxProductSubArray(int arr[]) {
+    int prod1 = arr[0],prod2 = arr[0],result = arr[0];
+    
+    for(int i=1;i<arr.length;i++) {
+        int temp = Math.max(arr[i],Math.max(prod1*arr[i],prod2*arr[i]));
+        prod2 = Math.min(arr[i],Math.min(prod1*arr[i],prod2*arr[i]));
+        prod1 = temp;
+        
+        result = Math.max(result,prod1);
+    }
+    
+    return result;
+	}
+	public static void main(String[] args) {
+		int nums[] = {1,2,-3,0,-4,-5};
+		int answer = maxProductSubArray(nums);
+		System.out.print("The maximum product subarray is: "+answer);
+	}
+}
+
+// Time Complexity: O(N)
+
+// Reason: A single iteration is used.
+
+// Space Complexity: O(1)
+
+// Reason: No extra data structure is used for computation
